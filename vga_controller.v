433d78cffc56737a743f83caef1d51d40591e01e
Chloe Kang <yk234@duke.edu>
2022-12-08 16:55:00 -0500

    update pipes

--- vga_controller.v
+++ vga_controller.v
@@ -23,7 +23,7 @@ output [7:0] r_data;
 ///////// ////                     
 reg [18:0] ADDR;
 reg [23:0] bgr_data;
-reg trigger;
+reg trigger, start_helper;
 wire VGA_CLK_n;
 wire [7:0] index;
 wire [23:0] bgr_data_raw;
@@ -64,23 +64,37 @@ counter <= 32'd0;
 end
 
 //////Add switch-input logic here
-reg [9:0] x,ADDRx;
-reg [8:0] y, ADDRy;
+reg [9:0] x, x_pipe, x_pipe2, ADDRx;
+reg [8:0] y, y_pipe, y_pipe2, ADDRy;
+reg [1:0] score;
+
 initial begin
  x <= 10'd100;
  y <= 9'd240;
+ x_pipe <= 10'd590;
+ y_pipe <= 9'd150;
+ score <= 2'd0;
+
+// x_pipe2 <= 10'd640;
+// y_pipe2 <= 9'd200;
  trigger <= 0;
+ start_helper <= 1;
  end
 
 always@(posedge iVGA_CLK)
 begin
-	if (!start) begin
+	if (!start && start_helper) begin
 		trigger <= 1;
+		start_helper <=0;
 	end
 	if (!reset) begin
 		x <= 10'd100;
 		y <= 9'd240;
+		x_pipe <= 10'd590;
+		y_pipe <= 9'd150;
 		trigger <= 0;
+		start_helper <= 1;
+		score <= 2'd0;
 	end
 	
 	if (trigger) begin
@@ -92,12 +106,41 @@ begin
 //	 x<=x - left;
 //	 else if(x == 0)
 //	 x<= x + right;
-	 if(y < 9'd450 && y > 9'd0)
+
+//		if(x_pipe <= 10'd320)begin
+//		x_pipe2 <= x_pipe2 - 1;
+//		end
+		x_pipe <= x_pipe - 1;
+		if (x_pipe-30 <= x && x_pipe + 50 >= x) begin
+			if (y_pipe > y ||  y_pipe + 180 - 30 < y) begin // gap: 180, bird height: 30
+//					x <= 10'd100;
+//					y <= 9'd240;
+//					x_pipe <= 10'd590;
+//					y_pipe <= 9'd150;
+					trigger <= 0;
+			end
+		end
+		
+		// the case when the pipe passed the bird
+		if (x_pipe + 50 < x) begin
+			score <= score + 1;
+		end
+		
+		if (y >= 9'd450 || y <= 0)begin
+//				x <= 10'd100;
+//				y <= 9'd240;
+//				x_pipe <= 10'd590;
+//				y_pipe <= 9'd150;
+				trigger <= 0;	 //lose
+	   end
+	  
+		
+	 if(y < 9'd450 && y > 9'd0) begin
 		y <= y - 4; // falling down
-		if (up)
-			y <= y + 1;
-		else if (y == 9'd450 && y == 0)
-			y <= y;	 //lose
+	 end
+	 if (up) begin
+		y <= y + 1;
+	 end
 	 
 	  
 	 end
@@ -139,10 +182,47 @@ always@(posedge iVGA_CLK)begin
 end
 
 always@(posedge VGA_CLK_n) begin
+	// score
+	if(score >= 2'd1 && ADDRx >= 20 && ADDRx < 30 && ADDRy < 30 && ADDRy > 20) begin
+		bgr_data <= 24'h6A0DAD;
+	end
+	
+//	if (score >= 2'd2) begin
+//		if(ADDRx >= 20 && ADDRx < 30 && (ADDRy < 60 && ADDRy > 50)) begin
+//				bgr_data <= 24'h6A0DAD;
+//		end
+//	end
+//	
+//	if (score >= 2'd3) begin
+//		if(ADDRx >= 20 && ADDRx < 30 && (ADDRy < 90 && ADDRy > 80)) begin
+//				bgr_data <= 24'h6A0DAD;
+//		end
+//	end
+//	
 
-	if(ADDRx >= x && ADDRx < x + 30 && ADDRy < y + 30 && ADDRy > y)begin
+	
+	// bird (block)
+	else if(ADDRx >= x && ADDRx < x + 30 && ADDRy < y + 30 && ADDRy > y)begin
+		bgr_data <= 24'h6A0DAD;
+	end
+	
+		// exclamation mark gan tan hao
+	else if(!start_helper && !trigger && ADDRx >= 295 && ADDRx < 345 && ((ADDRy < 300 && ADDRy > 50) || (ADDRy < 430 && ADDRy > 380))) begin
 		bgr_data <= 24'h000ABC;
 	end
+	
+	// first pipe
+	else if(ADDRx >= x_pipe && ADDRx < x_pipe + 50 && ((ADDRy < y_pipe && ADDRy > 0) || (ADDRy < 480 && ADDRy > y_pipe + 180))) begin
+		bgr_data <= 24'hFFFF00;
+	end
+	
+	
+//	
+//	// second pipe
+//	else if(ADDRx >= x_pipe2 && ADDRx < x_pipe2 + 50 && ((ADDRy < y_pipe2 && ADDRy > 0) || (ADDRy < 480 && ADDRy > y_pipe2 + 180))) begin
+//		bgr_data <= 24'hFFFF00;
+//	end 
+	// background
 	else begin
 		bgr_data <= bgr_data_raw;
 	end
